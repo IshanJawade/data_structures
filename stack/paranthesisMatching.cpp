9c74f59ae8de208755704b42e7b1052f3d74a3ea
Ishan Jawade <ishankjawade@gmail.com>
2020-07-21 01:19:58 +0530

    Paranthesis Matching added in Stack

--- stack/paranthesisMatching.cpp
+++ stack/paranthesisMatching.cpp
@@ -1,4 +1,39 @@
+// Paranthesis matching using stack 
+
+# include <bits/stdc++.h>   //includes built in stack function
 # include <iostream>
-# include <bits/stdc++.h>
-# include </Users/ishan/Desktop/c++/data_structures/stack/stackArray.cpp>
 using namespace std;
+
+void Check(string expression){
+    stack <int> s;      // declaration of "int" stack named "s".
+    int flg = 1;    // flag 
+    for(int i=0; i<expression.length(); i++){
+        if (expression[i] == '('){
+            s.push(1);
+        } else if (expression[i] == ')'){
+            if (s.empty() == 1){
+                cout<<"\nEqution is unbalanced"<<endl;
+                flg --;     //flag decreamented
+            }
+            else{
+            s.pop();
+            }
+        }
+    }
+    if (flg == 1){          //flag used here!
+        if(s.empty() == 1){
+            cout<<"\nEqution is balanced"<<endl;
+        }
+        else{
+            cout<<"\nEqution is unbalanced"<<endl;
+        }
+    }
+}
+
+int main(){
+    string expression = "(((a+b)-(c-d)))";
+    Check(expression);
+    return 0;
+}
+
+// Written by Ishan Jawade ✌🏻.
