107f383a8cfacfe7044c9035f0b622545c37f682
Ishan Jawade <ishan@Ishans-MacBook-Pro.local>
2020-11-08 23:22:50 +0530

    factorial code adde in basic concepts

---
+++ basicConcepts/factorial.cpp
@@ -0,0 +1,17 @@
+// to find the factorial of the number (recursion)
+
+# include <iostream>
+using namespace std;
+
+int factorial(int n){
+	if (n == 0)
+		return 1;
+	return n * factorial(n-1);
+}
+
+int main(){
+	int num;
+	cin >> num;
+	cout << factorial(num) << endl;
+	return 0;
+}
