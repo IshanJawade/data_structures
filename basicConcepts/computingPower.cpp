d4cd999f4454ce366afc4a5e1f814b838ed26acf
IshanJawade <ishankjawade@gmail.com>
2020-11-12 22:14:08 +0530

    Computing Power Code is Added

---
+++ basicConcepts/computingPower.cpp
@@ -0,0 +1,33 @@
+// Program to find out Powers of Entered Number 
+
+# include <iostream>
+using namespace std;
+
+int myPower(int y, int x){
+	int ans=1;
+	if (x == 1){
+		return y;
+	}
+	else if (x == 0){
+		return 1;
+	}
+	else{
+		for(int i=0; i<x; i++){
+			ans = ans * y;
+		}
+		return ans;
+	}
+
+}
+
+int main(){
+	system("clear");
+	int num, pow;
+	cout<<"Enter number: ";
+	cin >> num;
+	cout<<"Enter power: ";
+	cin >> pow;
+	cout << myPower(num, pow) <<  endl;
+	return 0;
+}
+
