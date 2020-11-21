7e6b5633f09fc97fa3d328b16fb3b107ba9c4f6e
Ishan Jawade <ishan@Ishans-MacBook-Pro.local>
2020-11-21 23:43:04 +0530

    ArrayOps is edited

--- algorithms/arrays/arrayOps.cpp
+++ algorithms/arrays/arrayOps.cpp
@@ -10,10 +10,13 @@ class arrayOps{
 
 int main(){
 	int arr[]={12, 34, 56, 78, 90, 97, 86, 64, 42, 31};
-	cout << "Address of first elemnts of array: " <<  arr ;
-	cout << "" ;
-	int len = *(&arr + 1) - arr; 		// *(&arr + 1) contains address of 
-	cout<<len;
+	cout <<  arr <<endl;
+
+	cout << &arr[0] << endl;
+	cout << arr[2] << endl;
+	cout << *(arr + 2) << endl;
+	int arrlen = *(&arr + 1) - arr; 		//formula to finding length of array
+	cout<<arrlen;
 	arrayOps obj;
 
 	return 0;
