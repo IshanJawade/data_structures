c4d1ec3212508862320c56d6f76237ab999ddf5d
Ishan Jawade <ishan@Ishans-MacBook-Pro.local>
2020-11-21 23:51:11 +0530

    code adde in arrayOps

--- algorithms/arrays/arrayOps.cpp
+++ algorithms/arrays/arrayOps.cpp
@@ -3,21 +3,32 @@ using namespace std;
 
 class arrayOps{
 	public:
-		int search(){
-
-		}
+		int search(int arr[], int arrlen, int num ){
+			for (int i = 0; i <= arrlen; i++){
+				if ( arr[i] == num){
+					return i;
+				}
+				else
+				{
+					cout<<"Number not found";
+				}
+				
+			}
+		}	
 };
 
 int main(){
+	arrayOps obj;
 	int arr[]={12, 34, 56, 78, 90, 97, 86, 64, 42, 31};
 	cout <<  arr <<endl;
-
 	cout << &arr[0] << endl;
 	cout << arr[2] << endl;
 	cout << *(arr + 2) << endl;
 	int arrlen = *(&arr + 1) - arr; 		//formula to finding length of array
-	cout<<arrlen;
-	arrayOps obj;
-
+	cout << arrlen;
+	int num;
+	cout << "Enter the number to search from the array:";
+	cin >> num;
+	obj.search(arr, arrlen, num);
 	return 0;
 }
