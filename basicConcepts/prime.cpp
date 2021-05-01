146a61eb5ffc6918b1e56e9d76009508d8c986c5
IshanJawade <ishankjawade@gmail.com>
2021-05-02 01:47:31 +0530

    prime.cpp added in repo

--- basicConcepts/prime.cpp
+++ basicConcepts/prime.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 bool checkPrime(int n){
-	
+    if(n == 2){
+        return true;
+    }
+	else if(n%2!=0 && n%3!=0 && n%5!=0){
+        return true;
+    }
+    return false;
 }
 
 int main(){
-	
+	cout<<"Enter the numnber: ";
+    int num;
+    cin>>num;
+    cout<<"Note: 1 = Prime 0 = Not Prime"<<endl;
+    cout<<"-> "<<checkPrime(num)<<endl;
+    return 0;
 }
