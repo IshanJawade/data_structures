92860573eea2c24c641b04be0fec73d1f569bcc4
IshanJawade <ishankjawade@gmail.com>
2026-03-12 18:05:00 -0700

    files changed direcotries

---
+++ arrays/testign_arrays.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std;
+
+void doubleIt(int nums[]);
+
+int main(){
+
+    int array[] = {0,1,2,3,4,5,6,7,8,9};
+    doubleIt(array);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << array[i] << " ";
+    }
+    
+    return 0;
+}
+
+void doubleIt(int nums[]){
+
+    for(int i=0; i<10; i++){
+        nums[i] = nums[i] * 2;
+    }
+
+}
